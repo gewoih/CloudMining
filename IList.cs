c86d2924f9a3da564df89ca6d266580cf1a5c9ea
Никита Раненко <nranenko@bk.ru>
2021-09-27 23:38:24 +0300

    .

---
+++ IList.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Windows.Controls;
+
+namespace CloudMining
+{
+	public interface IList<T>
+	{
+		public abstract List<T> items { get; set; }
+
+		public abstract void LoadList();
+		public abstract void DrawList(StackPanel uIElement);
+		public abstract GridView GenerateGridView();
+		public abstract DataTable GenerateDataTable();
+	}
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CloudMining.ViewModels
+{
+	class MainWindowViewModel
+	{
+	}
+}
