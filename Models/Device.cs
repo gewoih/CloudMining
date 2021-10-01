a628b18d4e5c193f07a95b493e1864f44df9eabe
Никита Раненко <nranenko@bk.ru>
2021-10-01 16:26:32 +0300

    Добавлена модель Device Добавлена DeviceViewModel Добавлен MembersControl

--- Models/Device.cs
+++ Models/Device.cs
@@ -8,16 +8,16 @@ namespace CloudMining.Models
 		public string workerName { get; set; }
 		public DateTime purchaseDate { get; set; }
 		public double hashrate { get; set; }
-		public int currencyId { get; set; }
+		public string currency { get; set; }
 		public int consumption { get; set; }
 
-		public Device(int _id, string _workerName, DateTime _purchaseDate, double _hashrate, int _currencyId, int _consumption)
+		public Device(int _id, string _workerName, DateTime _purchaseDate, double _hashrate, string _currency, int _consumption)
 		{
 			this.id = _id;
 			this.workerName = _workerName;
 			this.purchaseDate = _purchaseDate;
 			this.hashrate = _hashrate;
-			this.currencyId = _currencyId;
+			this.currency = _currency;
 			this.consumption = _consumption;
 		}
 	}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using CloudMining.Infrastructure.Commands;
+using CloudMining.Views;
+using System;
 using System.Windows;
 using System.Windows.Input;
 
@@ -7,27 +9,70 @@ namespace CloudMining.ViewModels
 	internal class MainWindowViewModel : BaseViewModel
 	{
 		#region Properties
-		private object _currentView;
-		public object CurrentView
+		private MembersViewModel _membersVM = new MembersViewModel();
+		public MembersViewModel MembersVM
 		{
-			get => _currentView;
-			set => Set(ref _currentView, value);
+			get => _membersVM;
+			set => Set(ref _membersVM, value);
+		}
+
+		private int _menuIndex = 0;
+		public int MenuIndex
+		{
+			get => _menuIndex;
+			set
+			{
+				Set(ref _menuIndex, value);
+
+				switch (MenuIndex)
+				{
+					case 1:
+						MembersVM.LoadMembersCommand.Execute(null);
+						MainPanelView = MembersVM;
+						break;
+					case 2:
+						break;
+				}
+			}
+		}
+
+		private object _mainPanelView;
+		public object MainPanelView
+		{
+			get => _mainPanelView;
+			set => Set(ref _mainPanelView, value);
 		}
 		#endregion
 
 		#region Constructor
 		public MainWindowViewModel()
 		{
+			ChangeMenuIndexCommand = new RelayCommand(OnChangeMenuIndexCommandExecuted, CanChangeMenuIndexCommandExecute);
 			CloseWindowCommand = new RelayCommand(OnCloseWindowCommandExecuted, CanCloseWindowCommandExecute);
 		}
 		#endregion
 
+		#region Methods
+
+
+
+		#endregion
+
 		#region Commands
 
-		#region CloseWindowCommand
+		#region CloseWindow
 		public ICommand CloseWindowCommand { get; }
 		private bool CanCloseWindowCommandExecute(object parameter) => true;
 		private void OnCloseWindowCommandExecuted(object parameter) => Application.Current.Shutdown();
+		#endregion
+
+		#region ChangeMenuIndex
+		public ICommand ChangeMenuIndexCommand { get; }
+		private bool CanChangeMenuIndexCommandExecute(object p) => true;
+		private void OnChangeMenuIndexCommandExecuted(object p)
+		{
+			MenuIndex = Convert.ToInt32(p);
+		}
 		#endregion
 
 		#endregion
