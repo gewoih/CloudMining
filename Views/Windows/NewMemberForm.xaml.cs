210b7d420a1c6c95b29855f1a389c9a0ba5c5c99
Никита Раненко <nranenko@bk.ru>
2021-10-04 23:59:51 +0300

    Создан функционал добавления новых участников

--- Views/Windows/NewMemberForm.xaml.cs
+++ Views/Windows/NewMemberForm.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,9 +18,33 @@ namespace CloudMining.Views.Windows
 	/// </summary>
 	public partial class NewMemberForm : Window
 	{
+		private Dictionary<string, int> membersTypes = new Dictionary<string, int>();
+
 		public NewMemberForm()
 		{
 			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			SQL connection = new SQL();
+
+			SqlDataReader reader = connection.Execute("select id, name from members_types_dict");
+			while (reader.Read())
+			{
+				this.membersTypes.Add(reader["name"].ToString(), Convert.ToInt32(reader["id"]));
+			}
+			MemberTypeCB.ItemsSource = this.membersTypes.Keys;
+		}
+
+		private void AddMemberButton_Click(object sender, RoutedEventArgs e)
+		{
+			SQL connection = new SQL();
+
+			if (!MemberNameTB.Equals(String.Empty) && !MemberTypeCB.SelectedIndex.Equals(-1) && MemberJoinDP.SelectedDate <= DateTime.Now)
+				connection.Execute("insert into members values(" + membersTypes[MemberTypeCB.Text] + ", '" + MemberNameTB.Text + "', " + "'" + MemberJoinDP.SelectedDate + "')");
+			else
+				MessageBox.Show("Введите корректные данные!");
 		}
 	}
 }
